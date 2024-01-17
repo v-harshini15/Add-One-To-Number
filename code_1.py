31871d9f3baf3ed31637fa50452509bf45e02cff
HARSHINI VINNAKOTA <82930198+v-harshini15@users.noreply.github.com>
2024-01-17 12:30:01 +0530

    code_1.py

--- code_1.py
+++ code_1.py
@@ -1,3 +1,6 @@
+#Given a non-negative number represented as an array of digits, add 1 to the number
+#( increment the number represented by the digits ).
+#The digits are stored such that the most significant digit is at the head of the list.
 def add_one(digits):
     n = len(digits)
     carry = 1
